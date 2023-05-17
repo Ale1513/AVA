25ffd9dfbaa523da5809ee3239147f917b7c8fc7
EdoardoZambernardi <132917328+EdoardoZambernardi@users.noreply.github.com>
2023-05-17 20:32:06 +0200

    17_05

--- js/script.js
+++ js/script.js
@@ -54,7 +54,106 @@ inputForm.onsubmit = (event) => {
 };
 ['restaurant', 'hotel', 'hospital', 'bus_station', 'bank', 'pharmacy']*/
 
-var nTocchi=0;
+var nTocchi=0;        
+var map;
+/*var username = document.getElementById("emailLog").value;
+var password = document.getElementById("pswLog").value;
+console.log(username);
+console.log(password);*/
+
+//------------- cerca ristoranti nelle vicinanze
+function onSound(){
+  if (nTocchi%2==0) 
+  {
+      var tag = '';
+      var tag_amenity = '';
+      if(document.getElementById("txtUtente").value.includes("ristoranti")){
+          tag = 'ristoranti';
+          tag_amenity = 'restaurant';
+      }
+      if(document.getElementById("txtUtente").value.includes("alberghi")){
+          tag = 'alberghi';
+          tag_amenity = 'hotel';
+      }
+      if(document.getElementById("txtUtente").value.includes("ospedali")){
+          tag = 'ospedali';
+          tag_amenity = 'hospital';
+      }
+      document.getElementById("logo").src="../img/logoSound.gif";
+      document.getElementById("txtUtente").style.display="none";
+      document.getElementById("btnUtente").value = "Stop";
+      if(document.getElementById("txtUtente").value.includes(`cerca ${tag} nelle vicinanze`)){
+          if (document.getElementById("map") && !document.getElementById("map").hasChildNodes()) {
+              map = L.map('map').setView([44.8015, 10.3279], 13);
+              L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+                  maxZoom: 18,
+              }).addTo(map);
+          }
+          // Otteniamo la posizione corrente dell'utente
+          navigator.geolocation.getCurrentPosition(function(position) {
+              var lat = position.coords.latitude;
+              var lon = position.coords.longitude;
+              // Costruiamo l'URL per cercare i ristoranti vicini alle coordinate ottenute
+              var serviziUrl = "https://overpass-api.de/api/interpreter?data=[out:json];node[amenity=" + tag_amenity + "](around:10000," + lat + "," + lon + ");out;";
+              // Eseguiamo la richiesta per ottenere i ristoranti
+              fetch(serviziUrl)
+              .then(response => response.json())
+              .then(data => {
+                  // Aggiungiamo i marker sulla mappa per ogni ristorante trovato
+                  for (var i = 0; i < data.elements.length; i++) {
+                  var servizio = data.elements[i];
+                  var marker = L.marker([servizio.lat, servizio.lon]).addTo(map);
+                  marker.bindPopup(servizio.tags.name);
+                  }
+              })
+          }, function(error) {
+              console.log("Errore durante la geolocalizzazione: ", error);
+          });
+      }
+  //-------------------------------------------------------------------------
+      if(document.getElementById("txtUtente").value.includes('promemoria')){
+        var socket = io();
+
+        let testo = document.getElementById("txtUtente").value;
+        socket.emit('newPromemoria', testo);   
+
+        socket.on('newPromemoria', function() {
+          document.getElementById("answer").innerText = "Promemoria inserito correttamente";
+        });
+      }
+
+      document.getElementById("txtUtente").value='';
+      nTocchi++;
+    
+  }
+
+  else
+  {
+      document.getElementById("logo").src="../img/logoSemplice.png";
+      if (map) {
+          map.remove();
+      }
+      document.getElementById("txtUtente").style.display="initial";
+      document.getElementById("btnUtente").value = "Invia";
+      nTocchi--;
+  }
+}
+
+function caricaProm(){
+  var socket = io();
+
+  socket.emit('tuttiProm');   
+  socket.on('tuttiProm', function(descriptions) {
+    const ulElement = document.createElement('ul');
+      descriptions.forEach((description) => {
+        const liElement = document.createElement('li');
+        liElement.textContent = description;
+        ulElement.appendChild(liElement);
+    });
+    document.getElementById("listaProm").appendChild(ulElement);
+  });
+}
 
 function goLogin(){
   document.getElementById("logSign").className="hidden";
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const http = require("http");
 const express = require('express');  
 const mysql = require('mysql');   
 const bcrypt = require('bcryptjs');   
-const jwt = require('jsonwebtoken');                                                 
 const app = express();            
 const server1 = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server1);
+
+var username;
+var password;
 
 const connection = mysql.createConnection({
   host: '127.0.0.1',
@@ -28,6 +30,8 @@ io.on('connection', (socket) => {
       socket.emit('signUp',null,null);
   });
   socket.on('login', (us, p) => {
+    username = us;
+    password = p;
     loginUser(us, p, function(result){
       if(result){
         console.log("OK");
@@ -37,6 +41,19 @@ io.on('connection', (socket) => {
         console.log("Errore");
       }
     })  
+  });
+  socket.on('newPromemoria', (testo) => {
+    insertNewPromemoria(username, password, testo);
+    socket.emit('newPromemoria');
+  });
+  socket.on('tuttiProm', () => {
+    getPromemoria(username,(err, descriptions) => {
+      if (err) {
+        console.error('Errore durante il recupero delle descrizioni dei promemoria:', err);
+        return;
+      }      
+      socket.emit('tuttiProm', descriptions);
+    });
   });
 });
 
@@ -75,6 +92,43 @@ async function insertNewUser(valore1, valore2){
   connection.query(sql, (err,result) => {
     if(err) throw err;
     console.log("record inserito");
+  });
+}
+
+function insertNewPromemoria(valore1, valore2, valore3){
+  let userId;
+  const queryId = `SELECT idUser FROM user WHERE username = '${valore1}'`;
+  
+  connection.query(queryId, (err, results) => {
+    if (err) throw err;
+  
+    if (results.length > 0) {
+      console.log(results);
+      userId = results[0].idUser;
+      console.log('ID utente:', userId);
+  
+      const queryInsert = `INSERT INTO promemoria (descrizione, idUser) VALUES ('${valore3}', ${userId})`;
+      connection.query(queryInsert, (err, results) => {
+        if (err) throw err;
+        console.log("record inserito");
+      });
+  
+    } else {
+      console.log('Utente non trovato');
+    }
+  });
+}
+
+function getPromemoria(valore1,callback){
+  const queryProm = `SELECT promemoria.descrizione FROM user INNER JOIN promemoria ON user.idUser = promemoria.idUser WHERE user.username = '${valore1}'`;
+  connection.query(queryProm, (err, results) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    const testi = results.map((row) => row.descrizione);
+    callback(null, testi);
   });
 }
 
